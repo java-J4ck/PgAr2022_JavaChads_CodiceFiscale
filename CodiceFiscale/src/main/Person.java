59c5b4c63d1b1847a09c6ef18c1067524b34f5c9
giacomo <giacomo.baresi.02@gmail.com>
2022-05-03 22:12:42 +0200

    messo nome inglese

--- CodiceFiscale/src/main/Person.java
+++ CodiceFiscale/src/main/Person.java
@@ -1,6 +1,6 @@
 package main;
 
-public class Persona {
+public class Person {
 	
 	private String name;
 	private String lastName;
@@ -9,7 +9,7 @@ public class Persona {
 	private Comune birthPlace;
 	private String fiscalCode;
 	
-	public Persona(String name, String lastName, char sex, String birthDay, Comune birthPlace, String fiscalCode) {
+	public Person(String name, String lastName, char sex, String birthDay, Comune birthPlace, String fiscalCode) {
 		this.name = name;
 		this.lastName = lastName;
 		this.sex = sex;
